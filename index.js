ff73cb493f2881ae5c95310e85a650ea5e43a936
jasonkiptoo <kiptoojason@gmail.com>
2022-08-05 17:43:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const eventsArray = [];
+
+function writeCards(names, eventName){
+
+    for (let i = 0; i < names.length; i++) {
+       
+        eventsArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        
+            }
+    return eventsArray;
+
+
+}
+
+
+
+function countDown (){
+
+let count=10;
+    
+    while (count >= 0) {
+        // console.log(`The countdown is @ ${count}`);
+
+        console.log(count)
+        count--;
+        
+      }
+    
+      
+   
+
+}
